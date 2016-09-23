787cf40319a63a837c22b42a74d10779d525f98b
Rob Roach <roberoach@hotmail.com>
2016-09-23 17:32:04 +0000

    no messgae

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -12,6 +13,7 @@ var osuRosterRoute = require('./routes/osuroster');
 var testroute = require('./routes/testroute');
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
