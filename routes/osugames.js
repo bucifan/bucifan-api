c9614e684cdb7d24ff31cec97fc823582eca9597
Rob Roach <roberoach@hotmail.com>
2016-09-26 17:55:06 +0000

    updated

--- routes/osugames.js
+++ routes/osugames.js
@@ -7,63 +7,127 @@ router.get('/', function(req, res, next) {
     y2016: [{
       date: "09/03",
       Opp: "Bowling Green",
-      OppShort: "BG"
+      OppShort: "BG",
+      Himg: "bowling_green1.png",
+      Start: "noon",
+      OSUScore: 77,
+      OppScore: 10,
+      loc: H
     }, {
       date: "09/10",
       Opp: "Tulsa",
-      OppShort: "Tulsa"
+      OppShort: "Tulsa",
+      Himg: "tulsa1.png",
+      Start: "noon",
+      OSUScore: 48,
+      OppScore: 3,
+      loc: H
     }, {
       date: "09/17",
       Opp: "Oklahoma",
-      OppShort: "Okl"
+      OppShort: "Okl",
+      Himg: "oklahoma1.png",
+      Start: "noon",
+      OSUScore: 45,
+      OppScore: 24,
+      loc: A
     },{ 
-      date: "09/17",
-      Opp: "Oklahoma",
-      OppShort: "Okl"
+      date: "10/01",
+      Opp: "Rutgers",
+      OppShort: "RTG",
+      Himg: "Rutgers1.png"
+      Start: "noon",
+      OSUScore: 0,
+      OppScore: 0,
+      loc: H
     },{ 
-      date: "09/17",
-      Opp: "Oklahoma",
-      OppShort: "Okl"
+      date: "10/08",
+      Opp: "Indiana",
+      OppShort: "Ind",
+      Himg: "Indiana1.png",
+      Start: "noon",
+      OSUScore: 0,
+      OppScore: 0,
+      loc: H
     },{ 
-      date: "09/17",
-      Opp: "Oklahoma",
-      OppShort: "Okl"
+      date: "10/15",
+      Opp: "Wisconsin",
+      OppShort: "WSC",
+      Himg: "Wisconsin1.png",
+      Start: "8pm",
+      OSUScore: 0,
+      OppScore: 0,
+      loc: H
     },{ 
-      date: "09/17",
-      Opp: "Oklahoma",
-      OppShort: "Okl"
+      date: "10/22",
+      Opp: "Penn State",
+      Start: "noon",
+      OppShort: "PSU",
+      Himg: "penn_st.png",
+      OSUScore: 0,
+      OppScore: 0,
+      loc: A
     },{ 
-      date: "09/17",
-      Opp: "Oklahoma",
-      OppShort: "Okl"
+      date: "10/29",
+      Opp: "Northwestern",
+      OppShort: "NW",
+      Himg: "Northwestern1.png",
+      Start: "5:30pm",
+      OSUScore: 0,
+      OppScore: 0,
+      loc: H
     },{ 
-      date: "09/17",
-      Opp: "Oklahoma",
-      OppShort: "Okl"
+      date: "11/05",
+      Opp: "Nebraska",
+      OppShort: "NB",
+      Himg: "Nebraska1.png",
+      Start: "8pm",
+      OSUScore: 0,
+      OppScore: 0,
+      loc: H
     },{ 
-      date: "09/17",
-      Opp: "Oklahoma",
-      OppShort: "Okl"
+      date: "11/12",
+      Opp: "Maryland",
+      OppShort: "MD",
+      Himg: "Maryland1.png",
+      Start: "noon",
+      OSUScore: 0,
+      OppScore: 0,
+      loc: A
     },{ 
-      date: "09/17",
-      Opp: "Oklahoma",
-      OppShort: "Okl"
+      date: "11/19",
+      Opp: "Michigan St",
+      OppShort: "MSU",
+      Himg: "michigan_st.png",
+      Start: "noon",
+      OSUScore: 0,
+      OppScore: 0,
+      loc: A
     },{ 
-      date: "09/17",
-      Opp: "Oklahoma",
-      OppShort: "Okl"
+      date: "11/26",
+      Opp: "TTUN",
+      OppShort: "TTUN",
+      Himg: "michigan1.png",
+      Start: "noon",
+      OSUScore: 0,
+      OppScore: 0,
+      loc: H
     },{ 
-      date: "09/17",
-      Opp: "Oklahoma",
-      OppShort: "Okl"
+      date: "12/02",
+      Opp: "Big Ten Championship",
+      Start: "8pm",
+      OppShort: "BTC",
+      OSUScore: 0,
+      OppScore: 0,
+      loc: H
     },{ 
-      date: "09/17",
-      Opp: "Oklahoma",
-      OppShort: "Okl"
-    },{ 
-      date: "09/17",
-      Opp: "Oklahoma",
-      OppShort: "Okl"
+      date: "12/31",
+      Opp: "Bowl Game",
+      OppShort: "BWL",
+      Start: "8pm",
+      OSUScore: 0,
+      OppScore: 0,
+      loc: H
     }]
   };
   res.json(gameJson);
